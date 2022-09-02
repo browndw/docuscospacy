0430a0db66248c32d7ee645038078185820437d4
browndw <davidwestbrown@gmail.com>
2022-09-02 10:06:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.2'
+VERSION = '0.1.4'
 PACKAGE_NAME = 'docuscospacy'
 AUTHOR = 'David Brown'
 AUTHOR_EMAIL = 'dwb2@andrew.cmu.edu'
