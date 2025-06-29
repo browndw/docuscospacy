5f3f99f4750bcafceadffdcacfa4541c053877c3
browndw <davidwestbrown@gmail.com>
2025-06-29 10:05:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,41 @@
 import pathlib
+import re
 from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.3.3'
-PACKAGE_NAME = 'docuscospacy'
-AUTHOR = 'David Brown'
-AUTHOR_EMAIL = 'dwb2@andrew.cmu.edu'
-URL = 'https://github.com/browndw/docuscospacy'
+with open("docuscospacy/__init__.py") as f:
+    init = f.read()
+    VERSION = re.search(r'__version__ = ["\']([^"\']+)["\']', init).group(1)
+    AUTHOR = re.search(r'__author__ = ["\']([^"\']+)["\']', init).group(1)
+    AUTHOR_EMAIL = re.search(r'__email__ = ["\']([^"\']+)["\']', init).group(1)
 
-LICENSE = 'Apache License 2.0'
-DESCRIPTION = 'Support for spaCy models trained on \
-      DocuScope and the CLAWS7 tagset'
+PACKAGE_NAME = "docuscospacy"
+URL = "https://github.com/browndw/docuscospacy"
+
+LICENSE = "Apache License 2.0"
+DESCRIPTION = "Support for spaCy models trained on \
+      DocuScope and the CLAWS7 tagset"
 LONG_DESCRIPTION = (HERE / "README.rst").read_text()
 LONG_DESC_TYPE = "text/x-rst"
 
 INSTALL_REQUIRES = [
-      'importlib-resources>=6.5.0',
-      'polars>=1.30.0',
-      'spacy>=3.8.0',
-      'scipy>=1.15.0'
+    "importlib-resources>=6.5.0",
+    "polars>=1.30.0",
+    "spacy>=3.8.0",
+    "scipy>=1.10.0",
 ]
 
-setup(name=PACKAGE_NAME,
-      version=VERSION,
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
-      long_description_content_type=LONG_DESC_TYPE,
-      author=AUTHOR,
-      license=LICENSE,
-      author_email=AUTHOR_EMAIL,
-      url=URL,
-      install_requires=INSTALL_REQUIRES,
-      packages=find_packages()
-      )
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    license=LICENSE,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(),
+)
